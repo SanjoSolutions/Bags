bb76ecae9ea6a53e5d3e65c167941bf0c331dbc4
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-12-23 17:57:27 +0100

    Reverts embedding approach

--- Bags.lua
+++ Bags.lua
@@ -1,58 +1,50 @@
-local addOnName = ...
-local version = '1.0.0'
+Bags = Bags or {}
 
-if not Library.isRegistered(addOnName, version) then
-  --- @class Bags
-  local Bags = Bags or {}
+local addOnName, AddOn = ...
+local _ = {}
 
-  Library.register(addOnName, version, Bags)
+function Bags.countItem(itemID)
+  local count = 0
 
-  local addOnName, AddOn = ...
-  local _ = {}
-
-  function Bags.countItem(itemID)
-    local count = 0
-
-    for containerIndex = 0, NUM_BAG_SLOTS + 1 do
-      for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
-        local itemInfo = Compatibility.Container.retrieveItemInfo(containerIndex, slotIndex)
-        if itemInfo and itemInfo.itemID == itemID then
-          count = count + itemInfo.stackCount
-        end
+  for containerIndex = 0, NUM_BAG_SLOTS + 1 do
+    for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
+      local itemInfo = Compatibility.Container.retrieveItemInfo(containerIndex, slotIndex)
+      if itemInfo and itemInfo.itemID == itemID then
+        count = count + itemInfo.stackCount
       end
     end
-
-    return count
   end
 
-  function Bags.findItem(itemIDs)
-    if type(itemIDs) == 'number' then
-      itemIDs = { itemIDs }
-    end
-    itemIDs = Set.create(itemIDs)
+  return count
+end
 
-    for containerIndex = 0, NUM_BAG_SLOTS + 1 do
-      for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
-        local slotItemID = C_Container.GetContainerItemID(containerIndex, slotIndex)
-        if Set.contains(itemIDs, slotItemID) then
-          return containerIndex, slotIndex
-        end
+function Bags.findItem(itemIDs)
+  if type(itemIDs) == 'number' then
+    itemIDs = { itemIDs }
+  end
+  itemIDs = Set.create(itemIDs)
+
+  for containerIndex = 0, NUM_BAG_SLOTS + 1 do
+    for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
+      local slotItemID = C_Container.GetContainerItemID(containerIndex, slotIndex)
+      if Set.contains(itemIDs, slotItemID) then
+        return containerIndex, slotIndex
       end
     end
-
-    return nil, nil
   end
 
-  function Bags.hasItem(itemID)
-    for containerIndex = 0, NUM_BAG_SLOTS + 1 do
-      for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
-        local slotItemID = C_Container.GetContainerItemID(containerIndex, slotIndex)
-        if slotItemID == itemID then
-          return true
-        end
+  return nil, nil
+end
+
+function Bags.hasItem(itemID)
+  for containerIndex = 0, NUM_BAG_SLOTS + 1 do
+    for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
+      local slotItemID = C_Container.GetContainerItemID(containerIndex, slotIndex)
+      if slotItemID == itemID then
+        return true
       end
     end
-
-    return false
   end
+
+  return false
 end
