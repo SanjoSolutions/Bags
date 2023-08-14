2a0295345d8e396567aca4dabcff2d2241ba9103
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-08-14 15:38:42 +0200

    Work

--- Bags.lua
+++ Bags.lua
@@ -1,66 +1,79 @@
-Bags = Bags or {}
+local addOnName = 'Bags'
+local version = '1.0.0'
 
-local addOnName, AddOn = ...
-local _ = {}
+if _G.Library and not Library.isRegistered(addOnName, version) then
+  local Compatibility = Library.retrieve('Compatibility', '^1.0.0')
+  local Set = Library.retrieve('Set', '^1.0.0')
 
-function Bags.countItem(itemID)
-  local count = 0
+  --- @class Bags
+  local Bags = {}
 
-  for containerIndex = 0, NUM_BAG_SLOTS + 1 do
-    for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
-      local itemInfo = Compatibility.Container.retrieveItemInfo(containerIndex, slotIndex)
-      if itemInfo and itemInfo.itemID == itemID then
-        count = count + itemInfo.stackCount
+  local addOnName, AddOn = ...
+  local _ = {}
+
+  function Bags.countItem(itemID)
+    local count = 0
+
+    for containerIndex = 0, NUM_BAG_SLOTS + 1 do
+      for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
+        local itemInfo = Compatibility.Container.retrieveItemInfo(containerIndex, slotIndex)
+        if itemInfo and itemInfo.itemID == itemID then
+          count = count + itemInfo.stackCount
+        end
       end
     end
+
+    return count
   end
 
-  return count
-end
+  function Bags.findItem(itemIDs)
+    if type(itemIDs) == 'number' then
+      itemIDs = { itemIDs }
+    end
+    itemIDs = Set.create(itemIDs)
 
-function Bags.findItem(itemIDs)
-  if type(itemIDs) == 'number' then
-    itemIDs = { itemIDs }
-  end
-  itemIDs = Set.create(itemIDs)
-
-  for containerIndex = 0, NUM_BAG_SLOTS + 1 do
-    for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
-      local slotItemID = C_Container.GetContainerItemID(containerIndex, slotIndex)
-      if Set.contains(itemIDs, slotItemID) then
-        return containerIndex, slotIndex
+    for containerIndex = 0, NUM_BAG_SLOTS + 1 do
+      for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
+        local slotItemID = C_Container.GetContainerItemID(containerIndex, slotIndex)
+        if Set.contains(itemIDs, slotItemID) then
+          return containerIndex, slotIndex
+        end
       end
     end
+
+    return nil, nil
   end
 
-  return nil, nil
-end
-
-function Bags.hasItem(itemID)
-  for containerIndex = 0, NUM_BAG_SLOTS + 1 do
-    for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
-      local slotItemID = C_Container.GetContainerItemID(containerIndex, slotIndex)
-      if slotItemID == itemID then
-        return true
+  function Bags.hasItem(itemID)
+    for containerIndex = 0, NUM_BAG_SLOTS + 1 do
+      for slotIndex = 1, Compatibility.Container.receiveNumberOfSlotsOfContainer(containerIndex) do
+        local slotItemID = C_Container.GetContainerItemID(containerIndex, slotIndex)
+        if slotItemID == itemID then
+          return true
+        end
       end
     end
+
+    return false
   end
 
-  return false
-end
-
-function Bags.determineNumberOfFreeSlots()
-  local numberOfFreeSlots = 0
-  for containerIndex = 0, NUM_BAG_SLOTS do
-    numberOfFreeSlots = numberOfFreeSlots + Compatibility.Container.receiveNumberOfFreeSlotsInContainer(containerIndex)
+  function Bags.determineNumberOfFreeSlots()
+    local numberOfFreeSlots = 0
+    for containerIndex = 0, NUM_BAG_SLOTS do
+      numberOfFreeSlots = numberOfFreeSlots + Compatibility.Container.receiveNumberOfFreeSlotsInContainer(containerIndex)
+    end
+    return numberOfFreeSlots
   end
-  return numberOfFreeSlots
-end
 
-function Bags.areBagsFull()
-  return Bags.determineNumberOfFreeSlots() == 0
-end
+  function Bags.areBagsFull()
+    return Bags.determineNumberOfFreeSlots() == 0
+  end
 
-function Bags.hasFreeSpace()
-  return Bags.determineNumberOfFreeSlots() >= 1
+  function Bags.hasFreeSpace()
+    return Bags.determineNumberOfFreeSlots() >= 1
+  end
+
+  Library.register(addOnName, version, Bags)
+else
+  error(addOnName + ' requires Library. It seems absent.')
 end
